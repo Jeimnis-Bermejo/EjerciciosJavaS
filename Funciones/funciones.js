16035d58ac904ef311125113e9f8183621d24399
Jeimnis_Bermejo <jeimnisbermejo@gmail.com>
2023-11-08 09:59:24 -0500

    funciones

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -1,14 +1,106 @@
 /*Ejercicio 1*/
 
-function sumar (numero1,numero2){
+/*function sumar (){
     console.log(100*20)
-    console.log (numero1,numero2)
 }
 
 
 
-/*Ejercicio 2*/
-sumar()
+/*Ejercicio 2
+sumar()*/
 
 /*Ejercicio 3*/
 
+/*function sumar (numero1,numero2){
+    console.log(numero1*numero2)
+}
+    */
+
+
+/*Ejercicio 4*
+
+function sumar( numero1,numero2){
+    console.log(numero1*numero2)
+}
+let numeroA=100
+let numeroB=20
+  /*sumar(numeroA,numeroB)
+  */
+
+/*Ejercicio 5*
+
+function sumar( numero1,numero2){
+    const resultado =100*20 
+    return resultado
+}
+ console.log (sumar(100,20))
+ 
+/*Ejercicio 6*
+
+const adicion =function(){
+    console.log(100*200)
+}
+adicion()*/
+
+/*Ejercicio 7*
+const adicion =()=>{
+    console.log(100*200)
+}
+adicion()*/
+
+/*Ejercicio 8*/
+
+function saludar(nombre) {
+    console.log(`Hola buenas tardes ${nombre}`)
+}
+saludar("Jeimnis")
+
+/*Ejercicio 9*/
+const multiplicacion = function (num1, num2) {
+    console.log(num1 * num2)
+}
+multiplicacion(20, 50)
+
+/*Ejercicio 10*/
+
+function area(base, altura) {
+
+    return base * altura
+}
+console.log(area(5, 6))
+
+function perimetro(a, b, c) {
+
+    return a + b + c
+}
+console.log(perimetro(3, 5, 6))
+
+//Ejercicio 11//
+ const total= (cantidad, precio)=>{
+return cantidad * precio
+}
+const cantidad = 10;
+const precio = 100;
+
+console.log(total(cantidad, precio))
+
+if (cantidad >= 20)
+
+ console.log((cantidad*precio)-(cantidad*precio)*(20/100))
+
+else if (cantidad >= 10)
+
+    console.log((cantidad*precio)-(cantidad*precio)*(10/100))
+else {
+    console.log(` No tienes descuento `);
+}
+
+//Ejercio 12//
+
+function esmayordeedad(edad){
+    console.log(edad)
+esmayordeedad(18)
+
+if (esmayordeedad>=18)
+console.log("Eres mayor de edad")
+}
